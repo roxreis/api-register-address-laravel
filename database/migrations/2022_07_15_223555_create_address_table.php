05d3edc1eb2a55118be96aed6dcf4e13a31c6b07
rodrigoreis <rodrigo.reis@westwing.com.br>
2022-07-15 19:56:13 -0300

    Address table created

---
+++ database/migrations/2022_07_15_223555_create_address_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->integer('zip_code');
+            $table->integer('number');
+            $table->string('street');
+            $table->string('complement')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
